9ca5133981b6fb4e841e0a6d0f4bb492315a4961
Nugencik <tox1c.1740@gmail.com>
2024-04-23 13:01:57 +0000

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main(){
-int t [4] = {5,2,6,4};
-    
+int t [5];
+cout<< "podaj 5 liczb a następnie kliknij enter"<<endl;
+	for(int i=0; i <5; i++ {
+    cin>>liczby[i];
 }
+    for(int i = 0; i < 5; i++0) {
+		for(int j = i +1; j < 5; j++) {
+			if (liczby[i] > liczby[j]) {
+				swap(liczby[i],liczby[j]);
+			
+		
+	
+cout 
--- cpp/cwiczenie2/main.cpp
+++ cpp/cwiczenie2/main.cpp
@@ -4,27 +4,21 @@ using namespace std;
 
 int main(){
 srand(time(NULL));
-    cout<<rand()<<endl;
-    cout<<rand()<<endl;
-//losowanie w zakresnie
-
+int liczba = 0;
 int ile_liczb_w_przedziale = 5;
 int startowa_liczba = 1;
 int wylosowana = rand() % ile_liczb_w_przedziale + startowa_liczba;
-cout<<wylosowana;
+do {
 
+cout<<"Podaj liczbe od 1 do 5:"<<endl;
+cin>>liczba;
+} while (liczba < 1 || liczba > 5);
 
-
-
-
-
-
-
-
-
-
-
-
+if(liczba==wylosowana)
+    cout<<"Twoja liczba jest taka sama"<<endl;
+else
+    cout<<"Twoja liczba nie jest taka sama"<<endl;
+cout<<wylosowana<<endl;
 
 
 
