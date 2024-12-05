7ce72127ab50bb891396a8f529915b175183eeef
Nugencik <student@student.machine>
2024-12-05 15:36:49 +0000

    studentmachine send

---
+++ cpp/wskaznik/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+/*                                    
+Prosty kalkulator z użyciem wskaźników
+twoim zadaniem jest napisanie programu który oblicza  sume i róznice  dwóch liczb za pomocą fukncji
+liczby oraz wyniki beda przekazywane do funkcji za pomocą wskaźników
+stwórz funkcje calculate przyjmuj a i b sum diff
+ */
+void calculate(float* n1, float* n2, float* sum, float* diff){
+    sum = n1 + n2;
+    diff = n1 - n2;
+}
+int main () {
+    float a, b;
+    float suma, roznica;
+cout<< "Podaj pierwsza liczbe: ";
+cin>> a;
+cout>> "Podaj drugą liczbe: ";
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
